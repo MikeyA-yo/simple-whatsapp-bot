8629547ae1445c0e52e7c09d2d2b614fd04642fe
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2024-04-04 10:22:58 +0100

    more utils

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const GroupChat = require('whatsapp-web.js/src/structures/GroupChat');
   const { GroupChat } = require('whatsapp-web.js')
   const qrcode = require('qrcode-terminal');
 const {lengthWords} = require('./word');
+const {menu} = require('./menu');
   const client = new Client({
       puppeteer: {
           browserWSEndpoint: await browser.wsEndpoint()
@@ -28,6 +29,21 @@ const {lengthWords} = require('./word');
 let s = '!sticker' || '!s'
   client.on('message', async msg => {
     if (msg.body.startsWith('!')) await  msg.react('😁');
+    if(msg.body == '!h' || msg.body == '!help' || msg.body == '!m'){
+      let commands;
+      menu.forEach(t =>{
+        commands += t
+        commands += '/\/->'
+      })
+      let text = `
+      Welcome, to this experiment,
+      let's build from here
+      commands🦾👇:
+      ${commands}
+      made with ❤🧡 by Ayomide(Mikey)
+      `
+     msg.reply(text);
+    }
       if (msg.body == '!ping') {
           msg.reply('pong');
       }else if (msg.body == '!x') {
@@ -38,7 +54,7 @@ let s = '!sticker' || '!s'
        await msg.reply(length);
       }else if (msg.body == s) {
              const chat = await msg.getChat();
-           if (!msg.hasMedia){
+           if (!msg.hasMedia && !msg.hasQuotedMsg){
                msg.reply('no pic provided')
            }else if (msg.hasMedia ){
             const media = await msg.downloadMedia();
@@ -47,6 +63,17 @@ let s = '!sticker' || '!s'
             stickerAuthor: 'Mikey',
             stickerName:'Bot'
            }) 
+         }else if(msg.hasQuotedMsg){
+          const quote = await msg.getQuotedMessage();
+          if (!quote.hasMedia) msg.reply('no pic provided');
+          if(quote.hasMedia){
+            const media = await quote.downloadMedia()
+            chat.sendMessage(media, {
+              sendMediaAsSticker: true,
+              stickerAuthor: 'Mikey',
+              stickerName:'Bot'
+             })
+          }
          }
       }else if(msg.body == '!invite'){
         //todo fix this crap /// done
@@ -62,7 +89,7 @@ let s = '!sticker' || '!s'
       }
       }else if (msg.body === '!everyone') {
         const chat = await msg.getChat();
-        
+        let content = msg.body.slice(10);
         let text = '';
         let mentions = [];
 
@@ -70,9 +97,67 @@ let s = '!sticker' || '!s'
             mentions.push(`${participant.id.user}@c.us`);
             text += `@${participant.id.user} `;
         }
+        const thing = `Message: ${content} 
+        ${text}`
 
-        await chat.sendMessage(text, { mentions });
-    }
+        await chat.sendMessage(thing, { mentions });
+    } else if (msg.body === '!leave') {
+      // Leave the group
+      let chat = await msg.getChat();
+      if (chat.isGroup) {
+          chat.leave();
+      } else {
+          msg.reply('This command can only be used in a group!');
+      }
+    } else if (msg.body.startsWith('!join ')) {
+      const inviteCode = msg.body.split(' ')[1].substring(26);
+      try {
+          await client.acceptInvite(inviteCode);
+          msg.reply('Joined the group!');
+      } catch (e) {
+          msg.reply('That invite code seems to be invalid.');
+      }
+    }else if (msg.body.startsWith('!add')){
+        const chat = await msg.getChat();
+        const numbers = msg.body.slice(5).split(" ");
+        numbers.forEach(no =>{
+          if(no.endsWith('@c.us')){
+            return no;
+          }else{
+            no +='@c.us';
+            return no;
+          }
+        })
+       const result = await chat.addParticipants(numbers);
+       for (let i = 0; i < numbers.length; i++){
+        let user = result.numbers[i];
+        msg.reply(user.message);
+       } 
+    }else if (msg.body === '!groupinfo') {
+      let chat = await msg.getChat();
+      if (chat.isGroup) {
+          msg.reply(`
+              *Group Details*
+              Name: ${chat.name}
+              Description: ${chat.description}
+              Created At: ${chat.createdAt.toString()}
+              Created By: ${chat.owner.user}
+              Participant count: ${chat.participants.length}
+          `);
+      } else {
+          msg.reply('This command can only be used in a group!');
+      }
+      }else if (msg.body === '!info') {
+         let info = client.info;
+         client.sendMessage(msg.from, `
+            *Connection info*
+             User name: ${info.pushname}
+             My number: ${info.wid.user}
+             Platform: ${info.platform}
+             Owner: Mikey(A-yo)
+             🙃🙃
+         `);
+     }
   });
 
   await client.initialize();
---
+++ menu.js
@@ -0,0 +1,17 @@
+const menu = [
+    '!m',
+    '!sticker',
+    '!everyone',
+    '!add',
+    '!wordlength',
+    '!invite',
+    '!ping',
+    '!leave',
+    '!join',
+    '!add',
+    '!info',
+    '!groupinfo'
+]
+module.exports={
+    menu
+}
